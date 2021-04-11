337f69d707f4a74e73c2f174b02fe31207911f4d
Sai <Sai@Sai-PC>
2021-04-11 18:27:25 +0530

    [shubham] reverse string

---
+++ java_practise/src/string/Test_reversestring.java
@@ -0,0 +1,17 @@
+package string;
+
+public class Test_reversestring{
+
+
+	public static void main(String[] args) {
+		
+		System.out.println(stringFormatter.reverseString("my name is khan"));
+		System.out.println(stringFormatter.reverseString("never back"));
+		
+
+
+		
+		
+	}
+
+}
